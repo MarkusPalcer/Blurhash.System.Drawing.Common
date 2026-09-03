6519a2103a8a20697419d72b5810dad71ef55f4e
Markus Palcer <markus.palcer@gmail.com>
2026-09-03 13:49:27 +0200

    add tests for encoding and decoding
    the latter needs to be checked manually

---
+++ Blurhash.System.Drawing.Common.Test/EncodingTests.cs
@@ -0,0 +1,31 @@
+using System.Drawing;
+using System.Drawing.Blurhash;
+using System.Runtime.Versioning;
+using FluentAssertions;
+using Xunit;
+
+namespace Blurhash.System.Drawing.Common.Test;
+
+[SupportedOSPlatform("Windows")]
+public class EncodingTests
+{
+    private const string BlurHash = "|HFFaXYk^6#M9vF~W@j=#*@-5b,1J5PBV=R:s;w[@[or[k6oO[TLtJrqnO};Fxi^OZE3NgM}sps,jMFxS#OtcXnzRjxZxHj]OYNeWGJCs9xunhwIXBIpNaxHNGr;v}aeo0XmxZXS$et6#*$ft6nhxHnNV@w{nOaKwfNHo0";
+
+    [SkippableFact]
+    public void TestEncoding()
+    {
+        var sourceImage = Image.FromFile("TestData/input.jpg");
+        var blurhash = Blurhasher.Encode(sourceImage, 9, 9);
+
+        blurhash.Should().Be(BlurHash);
+    }
+
+    [SkippableFact]
+    public void TestDecoding()
+    {
+        var targetImage = Blurhasher.Decode(BlurHash, 300, 200);
+        targetImage.Width.Should().Be(300);
+        targetImage.Height.Should().Be(200);
+        targetImage.Save("TestData/output.png");
+    }
+}
